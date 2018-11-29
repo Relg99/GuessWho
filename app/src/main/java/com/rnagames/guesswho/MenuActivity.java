d56cad99dda167f917e75abe7af6a272ef810cf2
Rleg99 <Rleg99@users.noreply.github.com>
2018-11-29 00:52:14 -0600

    v1.1

--- app/src/main/java/com/rnagames/guesswho/MenuActivity.java
+++ app/src/main/java/com/rnagames/guesswho/MenuActivity.java
@@ -1,8 +1,12 @@
 package com.rnagames.guesswho;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -12,18 +16,27 @@ import com.google.firebase.database.ValueEventListener;
 
 public class MenuActivity extends AppCompatActivity {
 
-    private static final String TAG = "MenuActivity";
+    private static final String TAG = "MENSAJEEEE";
+
+    public int res = 1;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        String nombreUsuario = "Ricardo";
 
         // Write a message to the database
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference("message");
+        DatabaseReference animalRef = database.getReference("Animal/Gato");
+        DatabaseReference crearPartidaRef = database.getReference("Juego/"+nombreUsuario);
 
-        myRef.setValue("Hello, World!");
+        animalRef.setValue("Blanco");
+        crearPartidaRef.setValue(1);
+        //myRef.setValue("Hello, World!");
 
         //Esto si es importante!
 
@@ -45,5 +58,23 @@ public class MenuActivity extends AppCompatActivity {
                 Log.w(TAG, "Failed to read value.", error.toException());
             }
         });
+    }
+
+
+    public void clikJugar (View view)
+    {
+        try {
+
+            Intent i = new Intent(this, LogInActivity.class);
+            i.putExtra("facnum", res);
+            Toast.makeText(this,""+res,Toast.LENGTH_SHORT).show();
+
+            startActivity(i);
+        }
+        catch (Exception e)
+        {
+            Toast.makeText(this,"Ingrese datos.",Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
