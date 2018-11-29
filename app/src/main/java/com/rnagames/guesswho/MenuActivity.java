c3a39e6afea00eb1c24681cd099e3e815e714ebe
Rleg99 <Rleg99@users.noreply.github.com>
2018-11-28 21:37:44 -0600

    v1.1

--- app/src/main/java/com/rnagames/guesswho/MenuActivity.java
+++ app/src/main/java/com/rnagames/guesswho/MenuActivity.java
@@ -38,6 +38,7 @@ public class MenuActivity extends AppCompatActivity {
             }
 
 
+
             @Override
             public void onCancelled(DatabaseError error) {
                 // Failed to read value
