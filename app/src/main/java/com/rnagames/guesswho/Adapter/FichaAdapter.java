f71b689f8ea5baa66f7608518505ed2eec0fbd16
yisusalanpng <45440937+yisusalanpng@users.noreply.github.com>
2018-12-04 02:19:40 -0600

    Personajes ya muestra los íconos de los personajes!!!!!!!!!!!!!!!!!

--- app/src/main/java/com/rnagames/guesswho/Adapter/FichaAdapter.java
+++ app/src/main/java/com/rnagames/guesswho/Adapter/FichaAdapter.java
@@ -3,77 +3,55 @@ package com.rnagames.guesswho.Adapter;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ImageView;
+
 
 import com.rnagames.guesswho.Pojos.Pojo_Personajes;
 import com.rnagames.guesswho.R;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
-public class FichaAdapter {
-    /*
+public class FichaAdapter extends RecyclerView.Adapter <FichaAdapter.FichaHolder>
+{
 
-package com.rnagames.guesswho.Adapter;
+    public ArrayList<Pojo_Personajes> datos;
+    public Context contexto;
 
-import android.content.Context;
-import android.support.annotation.NonNull;
-import android.support.v7.widget.RecyclerView;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
+    @NonNull
+    @Override
+    public FichaHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View vista = View.inflate(contexto, R.layout.adapter_personajes,null);
+        FichaHolder F =  new FichaHolder(vista);
+        return F;
+    }
 
-import com.rnagames.guesswho.Pojos.Pojo_Personajes;
-import com.rnagames.guesswho.R;
+    @Override
+    public void onBindViewHolder(@NonNull FichaHolder holder, int position) {
 
-import java.util.ArrayList;
+        Picasso.get()
+                .load(datos.get(position).getURL_Foto())
+                .into(holder.personaje);
+        Log.d("DEBUG","Llegue");
+    }
 
-    public class FichaAdapter extends RecyclerView.Adapter <FichaAdapter.FichaHolder>
+    @Override
+    public int getItemCount() {
+        return datos.size();
+    }
+
+    class FichaHolder extends RecyclerView.ViewHolder
     {
+        ImageView personaje;
 
-        public ArrayList<Pojo_Personajes> datos;
-        public Context contexto;
-
-        @NonNull
-        @Override
-        public FichaHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-            View vista = View.inflate(contexto,R.layout./* Aqui va el layout del boton unirse,null);
-            FichaHolder F =  new FichaHolder(vista);
-            return F;
-        }
-
-        @Override
-        public void onBindViewHolder(@NonNull FichaHolder holder, int position) {
-            holder.tvNombre.setText(datos.get(position).getNombre());
-            /*
-            Aqui va los datos que se pondran en la lista de unirse.
+        public FichaHolder(@NonNull View itemView) {
+            super(itemView);
+            personaje = itemView.findViewById(R.id.ivPersonaje);
 
 
-        }
-
-        @Override
-        public int getItemCount() {
-            return datos.size();
-        }
-
-        class FichaHolder extends RecyclerView.ViewHolder
-        {
-            TextView tvNombre,tvApellido,tvSexo,tvRegistro,tvDomicilio,tvFechaNacimiento,tvCelular,tvCP,tvColonia,tvMunicipio,tvCarrera;
-
-            public FichaHolder(@NonNull View itemView) {
-                super(itemView);
-                tvNombre = itemView.findViewById(R.id.tvNombre);
-                tvApellido = itemView.findViewById(R.id.tvApellido);
-
-                /*
-                Aqui va donde se pondra la informacion recopilada.
-
-
-
-            }
         }
     }
-    */
-
 }
--- app/src/main/java/com/rnagames/guesswho/PersonajesActivity.java
+++ app/src/main/java/com/rnagames/guesswho/PersonajesActivity.java
@@ -3,18 +3,103 @@ package com.rnagames.guesswho;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.flexbox.FlexDirection;
+import com.google.android.flexbox.FlexWrap;
+import com.google.android.flexbox.FlexboxLayoutManager;
+import com.google.android.flexbox.JustifyContent;
+import com.rnagames.guesswho.Adapter.FichaAdapter;
+import com.rnagames.guesswho.Pojos.Pojo_Personajes;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class PersonajesActivity extends AppCompatActivity {
 
-    public int res;
+    RecyclerView rvVista;
+    FichaAdapter fAdapter;
+    // Datos OrigenDatos;
+    LinearLayoutManager llmOrientacion;
+    ArrayList<Pojo_Personajes> arreglo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personajes);
 
-        Intent i = getIntent();
 
-        res = i.getIntExtra("facnum",-200);
+
+        fAdapter = new FichaAdapter();
+        rvVista = findViewById(R.id.rvVista);
+        //llmOrientacion = new LinearLayoutManager(this, LinearLayout.VERTICAL,false);
+        FlexboxLayoutManager layoutManager = new FlexboxLayoutManager(this);
+        layoutManager.setFlexDirection(FlexDirection.COLUMN);
+        layoutManager.setJustifyContent(JustifyContent.FLEX_START);
+        layoutManager.setFlexWrap(FlexWrap.WRAP);
+        arreglo = new ArrayList<>();
+
+        fAdapter.contexto = this;
+        fAdapter.datos=arreglo;
+        rvVista.setAdapter(fAdapter);
+        rvVista.setLayoutManager(layoutManager);
+        mostrar();
+
+
+
+    }
+
+    public void mostrar(){
+        JsonArrayRequest peticion = new JsonArrayRequest(
+                Request.Method.GET,
+                "https://guess-who-223421.appspot.com/selectFotos.php",
+                null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        for (int i = 0; i < response.length(); i++) {
+                            try {
+                                JSONObject jsonObject = response.getJSONObject(i);
+
+                                Pojo_Personajes personaje = new Pojo_Personajes();
+                                personaje.setURL_Foto(personaje.getURL_Foto()+jsonObject.getString("Foto"));
+
+                                //Toast.makeText(PersonajesActivity.this,personaje.getURL_Foto()+jsonObject.getString("Foto") , Toast.LENGTH_SHORT).show();
+
+                                arreglo.add(personaje);
+                                fAdapter.notifyDataSetChanged();
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+
+
+                        }
+                    }
+
+
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(PersonajesActivity.this,error.getMessage(),Toast.LENGTH_LONG).show();
+                    }
+                }
+        );
+
+        RequestQueue pide = Volley.newRequestQueue(this);
+        pide.add(peticion);
+
     }
 }
