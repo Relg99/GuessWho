bda1d960a990d94d2d9994625c6c0c61234d5880
Rleg99 <Rleg99@users.noreply.github.com>
2018-12-04 19:08:54 -0600

    v1.7  Vista dinamica para unirse a partidas

--- app/src/main/java/com/rnagames/guesswho/activity_lobby.java
+++ app/src/main/java/com/rnagames/guesswho/activity_lobby.java
@@ -3,6 +3,11 @@ package com.rnagames.guesswho;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -12,6 +17,8 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.rnagames.guesswho.Adapter.AdapterPartidas;
+import com.rnagames.guesswho.Datos.OrigenDatosPartidas;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -22,12 +29,26 @@ public class activity_lobby extends AppCompatActivity {
         String gamertag;
         TextView tvGamertag,tvNivel;
 
+        OrigenDatosPartidas Origen;
+        AdapterPartidas AdPartidas;
+        LinearLayoutManager llmOrientacion;
+        public RecyclerView rvListaPartidas;
         public int res ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lobby);
+
+        rvListaPartidas = findViewById(R.id.rvListaPartidas);
+        Origen = new OrigenDatosPartidas();
+        AdPartidas = new AdapterPartidas();
+        llmOrientacion = new LinearLayoutManager(this, LinearLayout.VERTICAL,false);
+        AdPartidas.datos = Origen.datosPartida();
+        AdPartidas.contexto = this;
+        rvListaPartidas.setLayoutManager(llmOrientacion);
+        rvListaPartidas.setAdapter(AdPartidas);
+
         tvGamertag=findViewById(R.id.tvGamertag);
         tvNivel=findViewById(R.id.tvNivel);
         Bundle recibirUsuario = getIntent().getExtras();
@@ -39,10 +60,10 @@ public class activity_lobby extends AppCompatActivity {
 
         res = i.getIntExtra("facnum",-200);
 
-
     }
 
-  public void getNivel(final String gamertag){
+
+    public void getNivel(final String gamertag){
 
       StringRequest postRequest = new StringRequest(Request.Method.POST, getNivelURL,
               new Response.Listener<String>()
@@ -74,5 +95,10 @@ public class activity_lobby extends AppCompatActivity {
       RequestQueue pide = Volley.newRequestQueue(this);
 
       pide.add(postRequest);
+  }
+
+  public void clickCrearPartida (View view)
+  {
+
   }
 }
