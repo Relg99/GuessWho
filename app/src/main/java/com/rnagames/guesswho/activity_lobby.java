330e21ae5237906f296ed85e287b57f7eca879a4
yisusalanpng <45440937+yisusalanpng@users.noreply.github.com>
2018-12-06 18:17:01 -0600

    Esta ahora sí

--- app/src/main/java/com/rnagames/guesswho/activity_lobby.java
+++ app/src/main/java/com/rnagames/guesswho/activity_lobby.java
@@ -96,6 +96,7 @@ public class activity_lobby extends AppCompatActivity {
                                 Intent i = new Intent(activity_lobby.this, activity_juego.class);
                                 i.putExtra("IdJuego", "" + IdJuego);
                                 i.putExtra("VistaTablero", VistaTablero);
+                                i.putExtra("tipoJugador", false);
 
                                 startActivity(i);
 
